f8178e5a717a0c207b89642648040584daf9ebdc
Isaac <isaac.thomson@yahoo.com>
2024-03-31 12:07:12 +1300

    list containing name, size and shipping option.

--- CodeSpace/Lists.py
+++ CodeSpace/Lists.py
@@ -1,7 +1,10 @@
-class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
-
-sams_score = class_name_test[2][1]
-print(sams_score)
-
-ellies_score = class_name_test[-1][-1]
-print(ellies_score)
+first_names = ["Ainsley", "Ben", "Chani", "Depak"]
+preferred_size = ["Small", "Large", "Medium"]
+preferred_size.append("Medium")
+customer_data = [["Ainsley", "Small", True], ["Ben", "Large", False], ["Chani", "Medium", True],
+                 ["Depak", "Medium", False]]
+customer_data[2][-1] = False
+customer_data[1].remove(False)
+new_customer_data = [["Amit", "Large", True], ["Karim", "X-Large", False]]
+customer_data_final = customer_data + new_customer_data
+print(customer_data_final)
